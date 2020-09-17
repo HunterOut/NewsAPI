559fa2bb9c68131cbb68eb33aa3c55c3cadcb458
hunter_out <hunter_out@icloud.com>
2020-09-17 15:33:30 +0300

    host

--- newsapi/settings/production.py
+++ newsapi/settings/production.py
@@ -20,13 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '95r3@(9lj(*2v9l^%0ym#z_dc*6tfw92vvpi5uz39ne9pyqmid'
-#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['newsapidevtoday.herokuapp.com']
 
 
 # Application definition
